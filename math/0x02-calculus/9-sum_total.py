333e3349becadcea97c224c30b909879c167d5b3
GandalfW <rafasua@gmail.com>
2021-07-22 23:57:40 +0000

    Primera funcion

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+import numpy as np
 
 def summation_i_squared(n):
-    if(isinstance(n,int)):
+    if(not isinstance(n,int)):
         return None
     else:
-        
+        x=range(1,(n+1))
+        x=np.power(x,2)
+        return sum(x)
 
 n=5
 print(summation_i_squared(n))
