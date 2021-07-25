6aff6d0c4bb8233967e2d4245ec73ebb3bf53634
GandalfW <rafasua@gmail.com>
2021-07-25 13:37:23 +0000

    Sinal de ejercicios

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Calculates polynomial integral
+"""
+def poly_integral(poly,c):
+    poly.insert(0,c)
+    i=0
+    for n in poly:
+        if i!=0:
+            poly[i]=n/i
+        i=i+1
+    return poly
