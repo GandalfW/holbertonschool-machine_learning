a79cb93a14d9a23443b0134e25c44fd251de5a56
GandalfW <rafasua@gmail.com>
2021-07-25 18:23:08 +0000

    Corrección de ejercicio 17

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -2,11 +2,14 @@
 """
 Calculates polynomial integral
 """
-def poly_integral(poly,c):
-    poly.insert(0,c)
-    i=0
+
+
+def poly_integral(poly, c=0):
+
+    poly.insert(0, c)
+    i = 0
     for n in poly:
-        if i!=0:
-            poly[i]=n/i
-        i=i+1
+        if i != 0:
+            poly[i] = n/i
+        i = i+1
     return poly
