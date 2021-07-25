db2ff089967a00a7afbe6e1941632f2424c2a878
GandalfW <rafasua@gmail.com>
2021-07-25 15:21:39 +0000

    corrección ejercicio 9 3 estilo

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+
 def summation_i_squared(n):
-    if(not isinstance(n,int)):
-        result=None
-    elif n==1:
-        result=1
+    if(not isinstance(n, int)):
+        result = None
+    elif n == 1:
+        result = 1
     else:
-        result=(pow(n,2)+summation_i_squared(n-1))
+        result = (pow(n, 2) + summation_i_squared(n - 1))
     return result
