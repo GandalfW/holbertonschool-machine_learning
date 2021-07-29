c72cf97057368075e3762ed0fe81a17a97dc2092
GandalfW <rafasua@gmail.com>
2021-07-29 03:28:14 +0000

    Ejercisio PMF

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -8,7 +8,6 @@ class Poisson:
     '''
     Poisson Distribution
     '''
-    e = 2.7182818285
 
     def __init__(self, data=None, lambtha=1.):
         '''
@@ -26,3 +25,25 @@ class Poisson:
                 raise ValueError("data must contain multiple values")
             else:
                 self.lambtha = sum(data) / len(data)
+
+    def pmf(self, k):
+        '''
+        Function PMF
+        '''
+
+        e = 2.7182818285
+        if k > 0:
+            if not isinstance(k, int):
+                k = int(k)
+        else:
+            return 0
+        tmp = range(0, k+1)
+        f = 1
+        for i in tmp:
+            if i == 0:
+                f = f * 1
+            else:
+                f = f * i
+        r = (e**(-self.lambtha))*(self.lambtha**k)
+        r = r/f
+        return r
