aa6f93d3f0c355e102270d03403277c8c75bebb8
GandalfW <rafasua@gmail.com>
2021-08-02 04:53:10 +0000

    me confie

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -36,3 +36,27 @@ class Normal:
         X Score
         '''
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        '''
+        Probaility Density Function
+        '''
+        e = 2.7182818285
+        pi = 3.1415926536
+        a = 1 / (self.stddev * ((2 * pi) ** (1 / 2)))
+        return a * (e ** (-(1 / 2) * ((x - self.mean) / self.stddev) ** 2))
+
+    def cdf(self, x):
+        '''
+        Cumulative Distribution Function
+        '''
+        e = 2.7182818285
+        pi = 3.1415926536
+        tmp = (x - self.mean) / (self.stddev * (2 ** (1 / 2)))
+        tmp2 = 2 / (pi ** (1 / 2))
+        a1 = (tmp ** 3) / 3
+        a2 = (tmp ** 5) /10
+        a3 = (tmp ** 7) / 42
+        a4 = (tmp ** 9) / 216
+        erf = tmp2 * (tmp - a1 + a2 - a3 + a4)
+        return (1 / 2) * (1 + erf)
