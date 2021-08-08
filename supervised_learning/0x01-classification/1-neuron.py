1d3b7b7d1c7ab72bb2ab1c07da057decf8be12dc
GandalfW <itdoctor@outlook.com>
2021-08-07 23:34:06 -0500

    Debbugin

--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -3,41 +3,42 @@
 Neuron Binary Classification
 '''
 
+
 class Neuron:
-        '''
-        Neuron Class
-        '''
+    '''
+    Neuron Class
+    '''
 
-        import numpy as np
+    import numpy as np
 
-        def __init__(self, nx):
-            '''
-            Init of the class
-            '''
-            if not isinstance(nx, int):
-                raise TypeError("nx must be a integer")
-            elif nx < 1:
-                raise ValueError("nx must be positive")
-            else:
-                self.nx = nx
-                self.__W = Neuron.np.random.randn(1 , nx)
-                self.__b = 0
-                self.__A = 0
-        
-        def W(self):
-            '''
-            Getter function for W
-            '''
-            return self.__W
-        
-        def A(self):
-            '''
-            Getter function for A
-            '''
-            return self.__A
-        
-        def b(self):
-            '''
-            Getter function for b
-            '''
-            return self.__b
+    def __init__(self, nx):
+        '''
+        Init of the class
+        '''
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        elif nx < 1:
+            raise ValueError("nx must be positive")
+        else:
+            self.nx = nx
+            self.__W = Neuron.np.random.normal(size=(1, nx))
+            self.__b = 0
+            self.__A = 0
+
+    def W(self):
+        '''
+        Getter function for W
+        '''
+        return self.__W
+
+    def A(self):
+        '''
+        Getter function for A
+        '''
+        return self.__A
+
+    def b(self):
+        '''
+        Getter function for b
+        '''
+        return self.__b
